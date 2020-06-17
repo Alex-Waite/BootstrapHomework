db48cbe3d7a7f70af5e8e3fe7678728003749388
Alex Waite <alexanderconorwaite@gmail.com>
2020-06-17 21:23:40 +0930

    adding more stuff to my portfilio page

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.single-item').slick({
+        // adaptiveHeight: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+    });
+
+});
